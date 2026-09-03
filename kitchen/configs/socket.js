fec41ce60efabf2a8b642561b82acefc3ed01b8e
Md Saquib <mdsaquib5540@gmail.com>
2026-09-03 18:53:03 +0530

    ok

--- kitchen/configs/socket.js
+++ kitchen/configs/socket.js
@@ -7,7 +7,7 @@ export const initSocket = (httpServer) => {
         cors: {
             origin: [
                 "http://localhost:3000",
-                "https://your-cloud-kitchen.vercel.app",
+                process.env.RESTAURANT_URL,
             ],
             credentials: true,
             methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
