a9348c66916d84dbfe96830d568d391c7327ee0b
Md Saquib <mdsaquib5540@gmail.com>
2026-09-06 00:21:21 +0530

    ok

--- kitchen/configs/cashfree.js
+++ kitchen/configs/cashfree.js
@@ -2,10 +2,11 @@ import { Cashfree, CFEnvironment } from "cashfree-pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-Cashfree.XClientId = process.env.CASHFREE_APP_ID;
-Cashfree.XClientSecret = process.env.CASHFREE_SECRET_KEY;
-Cashfree.XEnvironment = process.env.CASHFREE_ENVIRONMENT === "PRODUCTION" 
+const cashfree = new Cashfree();
+cashfree.XClientId = process.env.CASHFREE_APP_ID;
+cashfree.XClientSecret = process.env.CASHFREE_SECRET_KEY;
+cashfree.XEnvironment = process.env.CASHFREE_ENVIRONMENT === "PRODUCTION" 
     ? CFEnvironment.PRODUCTION 
     : CFEnvironment.SANDBOX;
 
-export default Cashfree;
+export default cashfree;
