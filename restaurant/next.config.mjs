7580ea2f01130ed147cd39c289ba2d857ec27b77
Md Saquib <mdsaquib5540@gmail.com>
2026-09-10 23:27:40 +0530

    ok

--- restaurant/next.config.mjs
+++ restaurant/next.config.mjs
@@ -3,6 +3,7 @@
 };
 
 nextConfig.images = {
+  unoptimized: true,
   remotePatterns: [
     { protocol: 'https', hostname: 'pub-b2fdf8a3ab70480aa2dce52f99ba6ed2.r2.dev' },
     { protocol: 'https', hostname: 'pub-863ef00e7a5f45a892803d4befa874c3.r2.dev' }
