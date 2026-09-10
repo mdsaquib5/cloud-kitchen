ca5dd33aee99c18b64f1843e362db8784916deee
Md Saquib <mdsaquib5540@gmail.com>
2026-09-10 23:25:14 +0530

    ok

--- restaurant/next.config.mjs
+++ restaurant/next.config.mjs
@@ -1,13 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
+﻿const nextConfig = {
   reactCompiler: true,
 };
 
 nextConfig.images = {
-    remotePatterns: [
-        { protocol: 'https', hostname: 'pub-b2fdf8a3ab70480aa2dce52f99ba6ed2.r2.dev' },
-        { protocol: 'https', hostname: 'pub-863ef00e7a5f45a892803d4befa874c3.r2.dev' }
-    ]
+  remotePatterns: [
+    { protocol: 'https', hostname: 'pub-b2fdf8a3ab70480aa2dce52f99ba6ed2.r2.dev' },
+    { protocol: 'https', hostname: 'pub-863ef00e7a5f45a892803d4befa874c3.r2.dev' }
+  ]
 };
 export default nextConfig;
