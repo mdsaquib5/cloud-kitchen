c49fa6a4c6c9c4f4cd35f9245174f2e4cda93aff
Md Saquib <mdsaquib5540@gmail.com>
2026-09-15 22:27:37 +0530

    okk

--- restaurant/src/app/checkout/page.jsx
+++ restaurant/src/app/checkout/page.jsx
@@ -511,14 +511,14 @@ const Checkout = () => {
                                     <span className="payment-name">Online Payment (UPI/Card)</span>
                                 </label>
 
-                                {/* <label
+                                <label
                                     className={`payment-option-label ${paymentMethod === "cash" ? "selected" : ""}`}
                                     onClick={() => setPaymentMethod("cash")}
                                 >
                                     <span className={`custom-radio ${paymentMethod === "cash" ? "checked" : ""}`}></span>
                                     <FiShoppingBag className="payment-icon" size={16} />
                                     <span className="payment-name">Cash on Delivery (COD)</span>
-                                </label> */}
+                                </label>
                             </div>
 
                             <button
